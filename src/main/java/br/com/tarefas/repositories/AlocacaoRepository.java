facd2180cd3eac6381c2266fdcf1df791532dfd1
Elkem Balieiro <elkem.balieiroo@gmail.com>
2022-06-23 23:25:29 -0300

    Outra commit

--- src/main/java/br/com/tarefas/repositories/AlocacaoRepository.java
+++ src/main/java/br/com/tarefas/repositories/AlocacaoRepository.java
@@ -3,6 +3,7 @@ package br.com.tarefas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+// import br.com.tarefas.models.Alocacao;
 import br.com.tarefas.models.Alocacao;
 
 @Repository
