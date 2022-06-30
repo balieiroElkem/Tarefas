bf6ceb701d8be93e4b50b1c6006219ab54883bf4
Elkem Balieiro <elkem.balieiroo@gmail.com>
2022-06-30 17:39:26 -0300

    Teste

--- src/main/java/br/com/tarefas/repositories/AlocacaoRepository.java
+++ src/main/java/br/com/tarefas/repositories/AlocacaoRepository.java
@@ -3,7 +3,6 @@ package br.com.tarefas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-// import br.com.tarefas.models.Alocacao;
 import br.com.tarefas.models.Alocacao;
 
 @Repository
